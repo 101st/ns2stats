311efc4144dbd990a1b9b470b5451fb6bc44631d
Aleksandr <3dspaun@gmail.com>
2017-01-17 20:15:59 +0500

    Добавил вывод статистики для отдельного игрока. В виде таблицы и графиков

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -176,10 +176,66 @@ app.controller('PlayerCTRL', function ($scope, $http, $log, $routeParams) {
 
 app.controller('PlayerTrackingDataCTRL', function ($scope, $http, $log, $routeParams) {
     $scope.playerDataArray = [];
+    var myChartsArray = [];
+
+    function createChart(ctx, data) {
+        myChartsArray[ctx] = new Chart(document.getElementById(ctx).getContext("2d"), {
+            type: 'line',
+            data: {
+                labels: data.labels,
+                datasets: [
+                    {
+                        label: "Skill",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: data.data,
+                        spanGaps: false
+                    }
+                ]
+            },
+            options: {
+                scaleOverride: true
+            }
+        });
+    }
+
+    function dateFormat(dateArray) {
+        var newArray = [];
+        _.each(dateArray, function (time) {
+            var newDate = new Date(time);
+            newArray.push((1 + newDate.getDay()) + '.' + newDate.getMonth() + '.' + newDate.getFullYear())
+        });
+        return newArray;
+    }
+
     $http.post('/get-player-tracking-data', {steamId: $routeParams.steamId})
         .then(function successCallback(response) {
             $log.log(response);
             $scope.playerDataArray = response.data;
+            var timeArray = dateFormat(_.pluck(response.data, 'update'));
+            var skillArray = _.pluck(response.data, 'skill');
+            var levelArray = _.pluck(response.data, 'level');
+            var scoreArray = _.pluck(response.data, 'score');
+            var xpArray = _.pluck(response.data, 'xp');
+            createChart('myChartSkill', {labels: timeArray, data: skillArray});
+            createChart('myChartLevel', {labels: timeArray, data: levelArray});
+            createChart('myChartScore', {labels: timeArray, data: scoreArray});
+            createChart('myChartXP', {labels: timeArray, data: xpArray});
         }, function errorCallback(err) {
             $log.log(err);
         });
